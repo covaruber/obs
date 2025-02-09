b8094ddc8e5551d73d68654069b36da6a5c93d8d
Giovanny Covarrubias <covaruberpaz@gmail.com>
2025-02-09 14:41:10 -0800

    Initial fix on plots

--- R/methods.R
+++ R/methods.R
@@ -1,6 +1,6 @@
 ##' @importFrom stats update
 ##' @S3method update evolaMod
-plot.sparsetMod <- function(object, y="expobject", lb=0, ub=Inf, box=TRUE, color="overlapMu", ...) {
+plot.sparsetMod <- function(object, y="expGain", lb=0, ub=Inf, box=TRUE, color="overlapMu", ...) {
   # if (is.null(call <- getCall(object))){stop("object should contain a 'call' component")}
   # call <- getCall(object)
   if(!inherits(object,"sparsetMod")){stop("not an object of class sparsetMod")}
@@ -11,7 +11,7 @@ plot.sparsetMod <- function(object, y="expobject", lb=0, ub=Inf, box=TRUE, color
   
   object$propMaxPlot <- round( (object$nPlots / max(object$nPlots))*100, 0)
   object$propMaxInds <- round( (object$nIndsAvail / max(object$nIndsAvail))*100, 0)
-  object$trt <- paste( addZeros(object$nIndsAvail),"i_", addZeros(object$nPlots ) , "p (",  object$propMaxInds,"% ;", object$propMaxPlot ,"%)", sep="")
+  object$trt <- paste( addZeros(object$nIndsAvail),"i_", addZeros(object$nPlots ) , "p ")#,  object$propMaxInds,"% ;", object$propMaxPlot ,"%)", sep="")
   
   object$colorBy <- object[,color]
   # make sure we color by averages
@@ -29,7 +29,7 @@ plot.sparsetMod <- function(object, y="expobject", lb=0, ub=Inf, box=TRUE, color
     p <- p + geom_violin( alpha = 0.7) 
   }
   
-  if(y=="accuracy"){myYlab <- "Accuracy [cor(true,estimated)]"}else if(y=="expobject"){myYlab <- "Expected genetic object (accuracy and intensity relationship)"}else{myYlab <- y}
+  if(y=="accuracy"){myYlab <- "Accuracy [cor(true,estimated)]"}else if(y=="expGain"){myYlab <- "Expected genetic gain (accuracy and intensity relationship)"}else{myYlab <- y}
   p <- p +  geom_point(position = position_jitter(seed = 1, width = 0.2), alpha=0.1) +
     scale_x_discrete(breaks=factor(object$trt), labels=object$trt) +
     facet_wrap(~nFarms, scales = "free_x") + #ylim(c(0,1)) +
@@ -38,7 +38,7 @@ plot.sparsetMod <- function(object, y="expobject", lb=0, ub=Inf, box=TRUE, color
                                                            barheight = 25, title.position = "bottom", 
                                                            title.hjust = 0.5)) +
     geom_hline(yintercept=myYint, linetype='dashed', color=c('red')) +
-    xlab("#Inds_#plots (%inds;#plots)") + 
+    xlab("#Inds_#plots ") + 
     ylab(myYlab) +
     labs(fill = color ) +
     scale_fill_gradient(low="forestgreen", high="red") 
